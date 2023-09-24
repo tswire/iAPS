02e1e5fdef5aa5dfe53319bec3b2872aee3c6815
Deniz Cengiz <48965855+dnzxy@users.noreply.github.com>
2023-09-25 01:41:19 +0200

    Add IOB and COB to calendar events (#220)
    * Add IOB and COB to calendar event

--- FreeAPS/Sources/Modules/DataTable/View/DataTableRootView.swift
+++ FreeAPS/Sources/Modules/DataTable/View/DataTableRootView.swift
@@ -125,6 +125,7 @@ extension DataTable {
                     .moveDisabled(true)
                 Text(item.type.name)
                 Text(item.amountText).foregroundColor(.secondary)
+
                 if let duration = item.durationText {
                     Text(duration).foregroundColor(.secondary)
                 }
