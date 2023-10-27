2d4c40dcad161ad785adf609c0bd94d4e4f864ef
Robert <comps@diprobe.com>
2023-10-27 17:35:59 +0200

    Streamline navigation views (#269)
    * Streamline home view navigation views:
    * Add title to carb entry view, as it was missing
    * Streamlines all buttons (centered alignment, same size) for all coming-from-home-view dialogs/views
    
    * Some changes after PR discussion
    * Change navigation title style from `.automatic` to `.inline` for all views accessible from home screen
    * Save screen space by moving (i) icon in bolus entry next to `Insulin recommended`
    * Change color styling for (i) icon in bolus entry
    
    * Revert info icon in bolus entry view back to speech bubble style"

--- FreeAPS/Sources/Modules/Home/View/HomeRootView.swift
+++ FreeAPS/Sources/Modules/Home/View/HomeRootView.swift
@@ -95,7 +95,7 @@ extension Home {
             .frame(maxWidth: .infinity)
             .padding(.top, 10 + geo.safeAreaInsets.top)
             .padding(.bottom, 10)
-            .background(Color.gray.opacity(0.2))
+            .background(Color.gray.opacity(0.3))
         }
 
         var cobIobView: some View {
@@ -421,7 +421,7 @@ extension Home {
             let colour: Color = colorScheme == .dark ? .black : .white
             // Rectangle().fill(colour).frame(maxHeight: 1)
             ZStack {
-                Rectangle().fill(Color.gray.opacity(0.2)).frame(maxHeight: 40)
+                Rectangle().fill(Color.gray.opacity(0.3)).frame(maxHeight: 40)
                 let cancel = fetchedPercent.first?.enabled ?? false
                 HStack(spacing: cancel ? 25 : 15) {
                     Text(selectedProfile().name).foregroundColor(.secondary)
