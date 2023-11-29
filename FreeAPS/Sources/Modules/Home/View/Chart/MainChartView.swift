c08e4c51d96b1d2f502cb3e2935bb910597d16ab
Robert <comps@diprobe.com>
2023-11-29 19:36:23 +0100

    resetting the semi-fixed chart limits

--- FreeAPS/Sources/Modules/Home/View/Chart/MainChartView.swift
+++ FreeAPS/Sources/Modules/Home/View/Chart/MainChartView.swift
@@ -41,8 +41,8 @@ struct MainChartView: View {
         static let topYPadding: CGFloat = 20
         static let bottomYPadding: CGFloat = 80
         static let minAdditionalWidth: CGFloat = 150
-        static let maxGlucose = 270
-        static let minGlucose = 0 // 45
+        static let maxGlucose = 240
+        static let minGlucose = 40
         static let yLinesCount = 5
         static let glucoseScale: CGFloat = 1 // default 2
         static let bolusSize: CGFloat = 8
