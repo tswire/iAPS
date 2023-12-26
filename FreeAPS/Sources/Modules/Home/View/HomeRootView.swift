8f9d505e6f779e18b2f97cd1136c3d04f50a13e2
dnzxy <d.c.cengiz@googlemail.com>
2023-12-26 10:03:43 -0500

    Add back timezone offset indicator that was mistakenly removed

--- FreeAPS/Sources/Modules/Home/View/HomeRootView.swift
+++ FreeAPS/Sources/Modules/Home/View/HomeRootView.swift
@@ -152,6 +152,7 @@ extension Home {
                 name: $state.pumpName,
                 expiresAtDate: $state.pumpExpiresAtDate,
                 timerDate: $state.timerDate,
+                timeZone: $state.timeZone,
                 state: state
             )
             .onTapGesture {
