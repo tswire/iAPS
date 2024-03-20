f6b5d860d2534b02f1b762bedb64f7dc95eec99b
Robert <comps@diprobe.com>
2024-03-20 12:03:19 +0100

    Bolus via shortcut config on top

--- FreeAPS/Sources/Modules/BolusCalculatorConfig/View/BolusCalculatorConfigRootView.swift
+++ FreeAPS/Sources/Modules/BolusCalculatorConfig/View/BolusCalculatorConfigRootView.swift
@@ -28,6 +28,45 @@ extension BolusCalculatorConfig {
             Form {
                 Section {
                     HStack {
+                        Toggle(isOn: $state.allowBolusShortcut) {
+                            Text("Allow iOS Bolus Shortcuts").foregroundStyle(state.allowBolusShortcut ? .red : .primary)
+                        }.disabled(isPresented)
+                            ._onBindingChange($state.allowBolusShortcut, perform: { _ in
+                                if state.allowBolusShortcut {
+                                    confirm = true
+                                    graphics = confirmButton()
+                                    info(
+                                        header: "Allow iOS Bolus Shortcuts",
+                                        body: "If you enable this setting you will be able to use iOS shortcuts and its automations to trigger a bolus in iAPS.\n\nObserve that the iOS shortuts also works with Siri!\n\nIf you need to use Bolus Shorcuts, please make sure to turn off the listen for 'Hey Siri' setting in iPhone Siri settings, to avoid any inadvertant activaton of a bolus with Siri.\nIf you don't disable 'Hey Siri' the iAPS bolus shortcut can be triggered with the utterance 'Hey Siri, iAPS Bolus'.\n\nWhen triggered with Siri you will be asked for an amount and a confirmation before the bolus command can be sent to iAPS.",
+                                        useGraphics: graphics
+                                    )
+                                }
+                            })
+                    }
+                    if state.allowBolusShortcut {
+                        HStack {
+                            Text(
+                                state.allowedRemoteBolusAmount > state.settingsManager.pumpSettings
+                                    .maxBolus ? "Max Bolus exceeded!" :
+                                    "Max allowed bolus amount using shortcuts "
+                            )
+                            .foregroundStyle(
+                                state.allowedRemoteBolusAmount > state.settingsManager.pumpSettings
+                                    .maxBolus ? .red : .primary
+                            )
+                            Spacer()
+                            DecimalTextField("0", value: $state.allowedRemoteBolusAmount, formatter: conversionFormatter)
+                        }
+                    }
+                } header: { Text("Allow iOS Bolus Shortcuts") }
+
+                Section {
+                    Toggle("Display Predictions", isOn: $state.displayPredictions)
+
+                } header: { Text("Smaller iPhone Screens") }
+
+                Section {
+                    HStack {
                         Toggle("Use alternate Bolus Calculator", isOn: $state.useCalc)
                     }
 
@@ -47,11 +86,6 @@ extension BolusCalculatorConfig {
                     }
                 } header: { Text("Calculator settings") }
 
-                Section {
-                    Toggle("Display Predictions", isOn: $state.displayPredictions)
-
-                } header: { Text("Smaller iPhone Screens") }
-
                 if state.useCalc {
                     Section {
                         HStack {
@@ -63,40 +97,6 @@ extension BolusCalculatorConfig {
                             DecimalTextField("0.7", value: $state.fattyMealFactor, formatter: conversionFormatter)
                         }
                     } header: { Text("Fatty Meals") }
-
-                    Section {
-                        HStack {
-                            Toggle(isOn: $state.allowBolusShortcut) {
-                                Text("Allow iOS Bolus Shortcuts").foregroundStyle(state.allowBolusShortcut ? .red : .primary)
-                            }.disabled(isPresented)
-                                ._onBindingChange($state.allowBolusShortcut, perform: { _ in
-                                    if state.allowBolusShortcut {
-                                        confirm = true
-                                        graphics = confirmButton()
-                                        info(
-                                            header: "Allow iOS Bolus Shortcuts",
-                                            body: "If you enable this setting you will be able to use iOS shortcuts and its automations to trigger a bolus in iAPS.\n\nObserve that the iOS shortuts also works with Siri!\n\nIf you need to use Bolus Shorcuts, please make sure to turn off the listen for 'Hey Siri' setting in iPhone Siri settings, to avoid any inadvertant activaton of a bolus with Siri.\nIf you don't disable 'Hey Siri' the iAPS bolus shortcut can be triggered with the utterance 'Hey Siri, iAPS Bolus'.\n\nWhen triggered with Siri you will be asked for an amount and a confirmation before the bolus command can be sent to iAPS.",
-                                            useGraphics: graphics
-                                        )
-                                    }
-                                })
-                        }
-                        if state.allowBolusShortcut {
-                            HStack {
-                                Text(
-                                    state.allowedRemoteBolusAmount > state.settingsManager.pumpSettings
-                                        .maxBolus ? "Max Bolus exceeded!" :
-                                        "Max allowed bolus amount using shortcuts "
-                                )
-                                .foregroundStyle(
-                                    state.allowedRemoteBolusAmount > state.settingsManager.pumpSettings
-                                        .maxBolus ? .red : .primary
-                                )
-                                Spacer()
-                                DecimalTextField("0", value: $state.allowedRemoteBolusAmount, formatter: conversionFormatter)
-                            }
-                        }
-                    } header: { Text("Allow iOS Bolus Shortcuts") }
 
                     Section {}
                     footer: { Text(
