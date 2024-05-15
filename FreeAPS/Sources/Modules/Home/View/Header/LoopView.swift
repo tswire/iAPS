231ceb888bf5b27deda81635ce5fa20455edbe9f
Robert <comps@diprobe.com>
2024-05-15 16:51:21 +0200

    LoopView pulse
    undo gradient
    
    Adjust LoopView scaling.

--- FreeAPS/Sources/Modules/Home/View/Header/LoopView.swift
+++ FreeAPS/Sources/Modules/Home/View/Header/LoopView.swift
@@ -2,6 +2,21 @@ import SwiftDate
 import SwiftUI
 import UIKit
 
+private var backgroundGradient: RadialGradient {
+    RadialGradient(
+        gradient: Gradient(colors: [
+            Color(red: 0.262745098, green: 0.7333333333, blue: 0.9137254902),
+            Color(red: 0.3411764706, green: 0.6666666667, blue: 0.9254901961),
+            Color(red: 0.4862745098, green: 0.5450980392, blue: 0.9529411765),
+            Color(red: 0.6235294118, green: 0.4235294118, blue: 0.9803921569),
+            Color(red: 0.7215686275, green: 0.3411764706, blue: 1)
+        ]),
+        center: .center,
+        startRadius: 27.0,
+        endRadius: 0.0
+    )
+}
+
 struct LoopView: View {
     private enum Config {
         static let lag: TimeInterval = 30
@@ -14,6 +29,7 @@ struct LoopView: View {
     @Binding var isLooping: Bool
     @Binding var lastLoopDate: Date
     @Binding var manualTempBasal: Bool
+    @State private var scale: CGFloat = 1.0
 
     private var dateFormatter: DateFormatter {
         let formatter = DateFormatter()
@@ -21,20 +37,23 @@ struct LoopView: View {
         return formatter
     }
 
-    private let rect = CGRect(x: 0, y: 0, width: 24, height: 24)
     var body: some View {
         VStack(alignment: .center) {
             ZStack {
-                Circle()
-                    .strokeBorder(color, lineWidth: 4)
-                    .frame(width: rect.width, height: rect.height, alignment: .center)
-                    .mask(mask(in: rect).fill(style: FillStyle(eoFill: true)))
                 if isLooping {
-                    ProgressView()
+                    PulsatingCircleView(color: color)
+                    /* ProgressView() */
+                } else {
+                    Circle()
+                        .strokeBorder(color, lineWidth: 4.5)
+                        .frame(width: 27, height: 27)
+                        .scaleEffect(1)
                 }
             }
             if isLooping {
-                Text("looping").font(.caption2)
+                /* Text("looping").font(.caption2) */
+                Text(timeString).font(.caption2)
+                    .foregroundColor(.secondary)
             } else if manualTempBasal {
                 Text("Manual").font(.caption2)
             } else if actualSuggestion?.timestamp != nil {
@@ -92,18 +111,48 @@ struct LoopView: View {
     }
 }
 
-extension View {
-    func animateForever(
-        using animation: Animation = Animation.easeInOut(duration: 1),
-        autoreverses: Bool = false,
-        _ action: @escaping () -> Void
-    ) -> some View {
-        let repeated = animation.repeatForever(autoreverses: autoreverses)
+/* extension View {
+     func animateForever(
+         using animation: Animation = Animation.easeInOut(duration: 1),
+         autoreverses: Bool = false,
+         _ action: @escaping () -> Void
+     ) -> some View {
+         let repeated = animation.repeatForever(autoreverses: autoreverses)
 
-        return onAppear {
-            withAnimation(repeated) {
-                action()
-            }
+         return onAppear {
+             withAnimation(repeated) {
+                 action()
+             }
+         }
+     }
+ } */
+
+struct PulsatingCircleView: View {
+    var color: Color
+    var size: CGFloat = 20.0
+    @State private var animate = false
+
+    var body: some View {
+        ZStack {
+//            Circle()
+//                .fill(color)
+//                .frame(width: 27, height: 27)
+//                .scaleEffect(animate ? 0.6 : 1.2)
+//                .animation(
+//                    Animation.easeInOut(duration: 1).repeatForever(autoreverses: true),
+//                    value: animate
+//                )
+            Circle()
+                .fill(backgroundGradient)
+                .frame(width: 27, height: 27)
+                .scaleEffect(animate ? 0.0 : 1.2)
+                .animation(
+                    Animation.easeInOut(duration: 2).repeatForever(autoreverses: true),
+                    value: animate
+                )
+        }
+        .onAppear {
+            self.animate = true
         }
     }
 }
